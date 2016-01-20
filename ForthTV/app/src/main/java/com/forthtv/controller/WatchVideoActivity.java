b99193fee71fda3475034399ccf83f992c57e443
Cuong Vo <baongoc@Baos-MacBook-Pro.local>
2016-01-20 13:58:05 +0700

    Implement: Watch video channel avatar

--- ForthTV/app/src/main/java/com/forthtv/controller/WatchVideoActivity.java
+++ ForthTV/app/src/main/java/com/forthtv/controller/WatchVideoActivity.java
@@ -2,19 +2,25 @@ package com.forthtv.controller;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.forthtv.BaseActivity;
 import com.forthtv.R;
 import com.forthtv.model.VideoData;
+import com.forthtv.util.PicassoCircleTransformation;
 import com.forthtv.util.view.VideoFooter;
 import com.forthtv.util.view.VideoHeader;
 import com.forthtv.util.view.VideoViewPlayer;
+import com.squareup.picasso.Picasso;
 
 /**
  * Created by baongoc on 1/12/16.
  */
 public class WatchVideoActivity extends BaseActivity {
     private VideoData mVideoData;
+
+    private ImageView mImageViewChannelAvatar;
+
     private VideoViewPlayer mVideoViewPlayer;
     private VideoHeader mVideoHeader;
     private VideoFooter mVideoFooter;
@@ -42,6 +48,11 @@ public class WatchVideoActivity extends BaseActivity {
                 // Need to release the media player also
             }
         });
+        mImageViewChannelAvatar = (ImageView)findViewById(R.id.activity_watch_channel_avatar);
+        Picasso.with(this).load(R.drawable.test_avatar_small)
+                .transform(new PicassoCircleTransformation())
+                .centerInside().resize(45,45)
+                .into(mImageViewChannelAvatar);
         mVideoData = (VideoData) getIntent().getSerializableExtra("VideoData");
         if(mVideoData != null){
             mVideoViewPlayer.setVideoData(mVideoData);
---
+++ ForthTV/app/src/main/java/com/forthtv/util/GraphicUtil.java
@@ -0,0 +1,7 @@
+package com.forthtv.util;
+
+/**
+ * Created by cuongvo on 1/20/16.
+ */
+public class GraphicUtil {
+}
